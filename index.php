79740d038dfbc13a0cc02d5d9e4b568f9716b1e4
Prhehns <pmalatuba@gmail.com>
2023-04-26 23:55:26 +0800

    FIrst commit

--- index.php
+++ index.php
@@ -19,7 +19,54 @@
             <header>
                 <h1>LOGO</h1>
                 <ul>
+                    <li>FORUM</li><!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+            <header>
+                <img src="images/webdev/logo.png" alt="">
+                <h1>Minecrafter</h1>
+                <ul>
                     <li>FORUM</li>
+                    <li>PROFILE</li>
+                    <li>SETTINGS</li>
+                    <li>SIGN/LOGOUT</li>
+                </ul>
+            </header>
+
+            <img class="home_pic" src="images/webdev/village.jpg" alt="">
+
+            <div class="container">
+                <h1>New Topics</h1>
+                <h2>Search</h2>
+                <div class="option_cont">
+                    <ul>
+                        <li>[Announcement]</li>
+                        <li>[Discussion]</li>
+                        <li>[Community]</li>
+                        <li>[Updates]</li>
+                        <li>[Mod Mail]</li>
+                        <li>[Constest]</li>
+                    </ul>
+                </div>
+            </div>
+        <script src="" async defer></script>
+    </body>
+</html>
                     <li>PROFILE</li>
                     <li>SETTINGS</li>
                     <li>SIGN/LOGOUT</li>
