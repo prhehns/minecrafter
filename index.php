8cc739826cf8a5248e20f43859381611ffe952f7
Prhehns <pmalatuba@gmail.com>
2023-04-27 21:15:43 +0800

    hotfix

--- index.php
+++ index.php
@@ -1,33 +1,37 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
-        <!--[if lt IE 7]>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <!--[if lt IE 7]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
-            <header>
-                <img src="images/webdev/logo.png" alt="">
-                <h1>Minecrafter</h1>
-                <ul>
-                    <li>FORUM</li>
-                    <li>PROFILE</li>
-                    <li>SETTINGS</li>
-                    <li>SIGN/LOGOUT</li>
-                </ul>
-            </header>
+    <header>
+        <img src="images/webdev/logo.png" alt="">
+        <h1>Minecrafter</h1>
+        <ul>
+            <li>FORUM</li>
+            <li>PROFILE</li>
+            <li>SETTINGS</li>
+            <li>SIGN/LOGOUT</li>
+        </ul>
+    </header>
 
-            <img class="home_pic" src="images/webdev/village.jpg" alt="">
+    <img class="home_pic" src="images/webdev/village.jpg" alt="">
 
-            <div class="container">
-                <h1>New Topics</h1>
-                <h2>Search</h2>
+    <div class="container">
+        <div class="container1">
+            <h1>New Topics</h1>
+            <h2>Search</h2>
+            <div class="container2">
                 <div class="option_cont">
                     <ul>
                         <li>[Announcement]</li>
@@ -38,7 +42,21 @@
                         <li>[Constest]</li>
                     </ul>
                 </div>
+                <div class="blogcont">
+                    <h1>blog1</h1>
+                    <h1>blog1</h1>
+                    <h1>blog1</h1>
+                    <h1>blog1</h1>
+                    <h1>blog1</h1>
+                </div>
             </div>
-        <script src="" async defer></script>
-    </body>
+
+        </div>
+    </div>
+    <footer>
+        <h1>@2023 CRAFTERS GUILD, INC. ALL RIGHTS RESERVE</h1>
+    </footer>
+    <script src="" async defer></script>
+</body>
+
 </html>
