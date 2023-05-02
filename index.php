c303907c5e6af2a30ec4f1afcac52689b465f2ca
cyril <cyrilreynold.trojillo@wvsu.edu.ph>
2023-05-02 16:02:20 +0800

    html hot fix

--- index.php
+++ index.php
@@ -11,9 +11,6 @@
 </head>
 
 <body>
-    <!--[if lt IE 7]>
-            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
     <header>
         <img src="images/webdev/logo.png" alt="">
         <h1>Minecrafter</h1>
