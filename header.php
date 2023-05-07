a58c88b63894c6c81f48a5d2e60b22fdaa6b5213
Prhehns <pmalatuba@gmail.com>
2023-05-07 21:40:38 +0800

    new files for login, signup, and !loggedin page

---
+++ header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <img src="images/webdev/logo.png" alt="">
+        <h1>Minecrafter</h1>
+        <ul>
+            <li> <a href="forum.php">FORUM</a> </li>
+            <li><a href="form.php">SIGN-UP</a></li>
+            <li><a href="login.php">LOGIN</a></li>
+        </ul>
+    </header>
+
+    <img class="home_pic" src="images/webdev/village.jpg" alt="">
+
+    <div class="container">
+        <div class="container1">
+            <h1>New Topics</h1>
+            <h2>Search</h2>
+            <div class="container2">
+                <div class="option_cont">
+                    <ul>
+                        <li>[Announcement]</li>
+                        <li>[Discussion]</li>
+                        <li>[Community]</li>
+                        <li>[Updates]</li>
+                        <li>[Mod Mail]</li>
+                        <li>[Constest]</li>
+                    </ul>
+                </div>
+                <div class="blogcont">
+                    <h1>blog1</h1>
+                    <h1>blog1</h1>
+                    <h1>blog1</h1>
+                    <h1>blog1</h1>
+                    <h1>blog1</h1>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <footer>
+        <h1>@2023 CRAFTERS GUILD, INC. ALL RIGHTS RESERVE</h1>
+    </footer>
+    <script src="" async defer></script>
+</body>
+
+</html>
